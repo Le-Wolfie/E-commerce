228248f4872a58e2a4458e437ee423b0332106bd
Amr Khaled <lewolfje@gmail.com>
2024-07-17 16:22:49 +0300

    add filtering

--- src/features/product/logic/handlers/getAllProducts.handler.ts
+++ src/features/product/logic/handlers/getAllProducts.handler.ts
@@ -2,10 +2,24 @@ import { Request, Response } from "express";
 import { ProductModel } from "../../../product/data/models/product.model";
 
 const getAllProductsHandler = async (req: Request, res: Response) => {
-  const products = await ProductModel.find({})
+  const { category, price } = req.query;
+
+  let filter: Record<string, any> = {};
+
+  if (category) {
+    filter.category = category;
+  }
+
+  if (price) {
+    // Assuming price is passed as a range in the format "min-max"
+    const [minPrice, maxPrice] = (price as string).split("-").map(Number);
+    filter.price = { $gte: minPrice, $lte: maxPrice };
+  }
+
+  const products = await ProductModel.find(filter)
     .skip(req.skip ?? 0)
     .limit(req.query.limit as unknown as number);
-  const total = await ProductModel.countDocuments({});
+  const total = await ProductModel.countDocuments(filter);
 
   const response = {
     products: products.map((product) => ({
