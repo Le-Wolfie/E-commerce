f5d8edd6c083c5a03def90faa266dd2dd4d4dfc6
Amr Khaled <lewolfje@gmail.com>
2024-07-18 14:08:08 +0300

    add admin nav and pages

---
+++ e-commerce/app/(authenticated)/admin/layout.tsx
@@ -0,0 +1,31 @@
+import { Navbar, NavLink } from "@/components/Navbar";
+import { tokenPayload } from "@/lib";
+import { Role } from "@/models/checkRole.middleware";
+import { getServerSession } from "next-auth";
+
+export const dynamic = "force-dynamic";
+
+export default async function Layout({
+  children,
+  params: { locale },
+}: Readonly<{
+  children: React.ReactNode;
+  params: { locale: string };
+}>) {
+  const session = await getServerSession();
+  const role = tokenPayload(session).role;
+  if (role !== Role.ADMIN) {
+    return <div>Unauthorized</div>;
+  }
+  return (
+    <>
+      <Navbar>
+        <NavLink href={`/admin`}>Dashboard</NavLink>
+        <NavLink href={`/admin/products`}>Products</NavLink>
+        <NavLink href={`/admin/orders`}>Orders</NavLink>
+        <NavLink href={`/admin/users`}>Customers</NavLink>
+      </Navbar>
+      <div className='container my-6'>{children}</div>
+    </>
+  );
+}
