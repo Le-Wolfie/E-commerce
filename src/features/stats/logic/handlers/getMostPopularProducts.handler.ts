dd09abca100448a3515a95323fe7e0f32d11811d
Amr Khaled <lewolfje@gmail.com>
2024-07-19 11:09:38 +0300

    add most popular product route

---
+++ src/features/stats/logic/handlers/getMostPopularProducts.handler.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { ProductModel } from "../../../product/data/models/product.model";
+import { OrderModel } from "../../../checkout/data/models/order.model";
+
+type HandlerRequest = Request<{}, {}, {}>;
+
+const getMostPopularProductsHandler = async (
+  req: HandlerRequest,
+  res: Response
+) => {
+  // Get the products that have been ordered the most
+  const mostPopularProducts = await OrderModel.aggregate([
+    { $unwind: "$items" },
+    {
+      $group: {
+        _id: "$items.product",
+        count: { $sum: "$items.quantity" },
+      },
+    },
+    { $sort: { count: -1 } },
+    { $limit: 5 },
+  ]);
+
+  const productIds = mostPopularProducts.map((product) => product._id);
+
+  const products = await ProductModel.find(
+    { _id: { $in: productIds } },
+    {
+      __v: 0,
+      updatedAt: 0,
+      createdAt: 0,
+    }
+  );
+
+  const response = {
+    products,
+  };
+
+  res.status(200).json(response);
+};
+
+export default getMostPopularProductsHandler;
